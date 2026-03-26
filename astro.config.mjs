835d268dabcddf978ec60c813f23e1382442e13f
Quinn Salas <11217831+darkhist@users.noreply.github.com>
2026-03-26 16:03:40 -0500

    feat: v1.1.0
    chore(deps): bump astro to 7.1.1
    feat(blog): add london 2026 post
    feat(perf): optimize LCP

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,15 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "Inter",
-        cssVariable: "--font-inter",
-        weights: ["400", "500", "600", "700"],
-        featureSettings: "'zero', 'ss01'",
-        fallbacks: ["sans-serif"],
-        subsets: ["latin"],
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      weights: ["400", "500", "600", "700"],
+      featureSettings: "'zero', 'ss01'",
+      fallbacks: ["sans-serif"],
+      subsets: ["latin"],
+    },
+  ],
 });
